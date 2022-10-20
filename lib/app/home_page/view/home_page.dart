285f293c471d2e7edd6c2bb38bbc353ed38e187f
sayanth A <sayanthsayu619@gmail.com>
2022-10-20 17:18:36 +0530

    slider added

--- lib/app/home_page/view/home_page.dart
+++ lib/app/home_page/view/home_page.dart
@@ -1,6 +1,11 @@
+import 'package:banglore_assignement/app/home_page/view_model/view_model.dart';
+import 'package:banglore_assignement/app/utiles/colors.dart';
+import 'package:banglore_assignement/app/utiles/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,13 +16,39 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
+            height20,
             Row(
               children: const [
                 Icon(Icons.navigation),
                 Text("Location not updated"),
               ],
             ),
-
+            ImageSlideshow(
+              indicatorBackgroundColor: Colors.transparent,
+              indicatorColor: white,
+              indicatorRadius: 5,
+              isLoop: true,
+              autoPlayInterval: 4000,
+              children: List.generate(
+                list.length,
+                (index) {
+                  final data = list[index];
+                  return Container(
+                    margin: const EdgeInsets.all(10),
+                    height: 80,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: white,
+                      image: DecorationImage(
+                        image: NetworkImage(data),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/app/home_page/view_model/view_model.dart
@@ -0,0 +1,14 @@
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class HomePageController extends GetxController{
+  
+
+
+}
+final list = [
+  'https://img.freepik.com/premium-vector/life-insurance-offer-landscape-banner-design-template_262129-10931.jpg?w=2000',
+  'https://img.freepik.com/free-vector/health-insurance-isometric-landing-page-banner_107791-2781.jpg?w=2000',
+  'https://mobiosolutions.com/wp-content/uploads/2022/05/Things-You-Should-Know-About-On-Demand-Insurance-App-Development.png',
+  'https://phonepe.com/blog_images/2017/march/25/take_control_pp/01_take_control_pp_banner.png'
+  
+];
