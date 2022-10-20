533c76adfe2501904dbe3bc1990a154cafbb8042
sayanth A <sayanthsayu619@gmail.com>
2022-10-20 16:35:28 +0530

    colors added

---
+++ lib/app/utiles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+const Color yellow = Colors.yellow;
+const Color white = Color.fromARGB(255, 255, 255, 255);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color red = Color.fromARGB(255, 247, 0, 0);
